6d1704c33deac2802a15a4315bc16b6cd82fde5a
Sandeep Urankar <66010653+SandeepUrankar@users.noreply.github.com>
2023-04-19 04:00:53 +0000

    Add exception for story not found.

--- stories.py
+++ stories.py
@@ -65,25 +65,25 @@ def get_story():
     _db = get_db()
     data = request.json
     
-    # try:
-    bookno = data['bookno']
-    story = {"story": stories[bookno]['content'], "bookno":bookno}
-    username = data['username']
-    _users = _db['users'].find()
-    users = [{"id": user["_id"], "username": user["username"], "password": user["password"], "history": user['history']} for user in _users]
-    user = None
-    for _user in users:
-        if _user['username'] == username:
-            user = _user
-    print('------------HERE---------------')
-    print(user)
-    history = user['history']
-    print(type(history))
-    history.append(bookno)
-    _db['users'].update_one({"username":username}, {'$set':{"history": history}})
-    return jsonify(story)
-    # except KeyError:
-    #     return jsonify({"msg":"Error"})
+    try: 
+        bookno = data['bookno']
+        story = {"story": stories[bookno]['content'], "bookno":bookno}
+        username = data['username']
+        _users = _db['users'].find()
+        users = [{"id": user["_id"], "username": user["username"], "password": user["password"], "history": user['history']} for user in _users]
+        user = None
+        for _user in users:
+            if _user['username'] == username:
+                user = _user
+        print('------------HERE---------------')
+        print(user)
+        history = user['history']
+        print(type(history))
+        history.append(bookno)
+        _db['users'].update_one({"username":username}, {'$set':{"history": history}})
+        return jsonify(story)
+    except KeyError:
+        return jsonify({"msg":"Error, story not found."})
 
 
 # @app.route('/signup', methods=['POST'])
